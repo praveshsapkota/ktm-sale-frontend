fb88e51b5ee2a1334bb3212f7857e89ef0cd1bac
Ethan_rogue <1da18cs115.cs@drait.edu.in>
2021-10-16 23:30:08 +0545

    made some changes in apollo client setting to send auth header

--- src/utils/ApolloClient.ts
+++ src/utils/ApolloClient.ts
@@ -1,8 +1,49 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { createUploadLink } from "apollo-upload-client"
-const uri = "http://localhost:4000/graphql"
-export const client = new ApolloClient({
-	uri: uri,
-	cache: new InMemoryCache(),
-	link: createUploadLink({ uri: uri }) as any
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloLink,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+import { onError } from "@apollo/client/link/error";
+
+const uri = "http://localhost:4000/";
+
+const authMiddleware = new ApolloLink( (operation, forward) => {
+	// add the authorization to the headers
+	
+	operation.setContext(({ headers = {} }) => ({
+	headers: {
+		...headers,
+		authorization: localStorage.getItem("Token") || ""
+	}
+	}));
+	
+	return forward(operation);
 });
+
+const UploadLink = createUploadLink({
+	uri: uri,
+	// headers: {
+	// 	jwtTokens:  Token()
+	// },
+}) as any ;
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors)
+		graphQLErrors.forEach(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
+
+	if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+export const client = new ApolloClient({
+	// uri: uri,
+	cache: new InMemoryCache(),
+	link: ApolloLink.from([errorLink,authMiddleware,UploadLink]),
+	credentials : "include"
+
+});
+
